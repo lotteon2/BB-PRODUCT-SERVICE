36dc164a8294adb24e5e6b34d4d85fbecf9394c2
nowgnas <dev.nowgnas@gmail.com>
2023-11-21 14:37:51 +0900

    :card_file_box: Add table

--- src/main/java/kr/bb/product/entity/Category.java
+++ src/main/java/kr/bb/product/entity/Category.java
@@ -1,14 +1,10 @@
 package kr.bb.product.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,20 +17,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Entity
-@Table(name = "review_images")
-public class ReviewImages extends BaseEntity {
+@Table(name = "category")
+public class Category extends BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "review_images_id")
+  @Column(name = "category_id")
   private Long id;
 
-  @Column(name = "review_image_url")
-  private String reviewImageUrl;
-
-  @ManyToOne(
-      fetch = FetchType.LAZY,
-      cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-  @JoinColumn(name = "review_id")
-  private Review review;
-
+  @Column(name = "category_name")
+  private String categoryName;
 }
