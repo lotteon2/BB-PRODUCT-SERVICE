1a8d08be9ce3ebcd37a7516f8836783e7eea2663
nowgnas <dev.nowgnas@gmail.com>
2023-11-21 12:01:41 +0900

    :card_file_box: LF1-286 #close add aws dynamodb config

---
+++ src/main/java/kr/bb/product/entity/Flowers.java
@@ -0,0 +1,17 @@
+package kr.bb.product.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Flowers {
+  private Long flowerId;
+  private String flowerName;
+  private Long stock;
+}
