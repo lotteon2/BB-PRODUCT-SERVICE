f380667b1518b9753bafef7d70dc912c8461dd09
nowgnas <dev.nowgnas@gmail.com>
2023-11-23 15:28:12 +0900

    :bug: Add request dto NotBlank

--- src/main/java/kr/bb/product/dto/request/ProductRequestData.java
+++ src/main/java/kr/bb/product/dto/request/ProductRequestData.java
@@ -1,6 +1,7 @@
 package kr.bb.product.dto.request;
 
 import java.util.List;
+import javax.validation.constraints.NotBlank;
 import kr.bb.product.vo.FlowersRequestData;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,15 +14,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ProductRequestData {
-  private String productName;
-  private String productSummary;
-  private String productDescriptionImage;
-  private String productThumbnail;
-  private Long productPrice;
+  @NotBlank private String productName;
+  @NotBlank private String productSummary;
+  @NotBlank private String productDescriptionImage;
+  @NotBlank private String productThumbnail;
+  @NotBlank private Long productPrice;
   private Long categoryId;
-  private Long storeId;
+  @NotBlank private Long storeId;
   private List<Long> productTag;
-  private FlowersRequestData representativeFlower;
+  @NotBlank private FlowersRequestData representativeFlower;
   private List<FlowersRequestData> flowers;
 
   public void setStoreId(Long storeId) {
