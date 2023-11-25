a2d3b672298a3ec6850960947c678fcc2ce3fcf5
nowgnas <dev.nowgnas@gmail.com>
2023-11-25 12:56:32 +0900

    :arrow_up: Update dependency

--- src/main/java/kr/bb/product/exception/errors/ProductNotFoundException.java
+++ src/main/java/kr/bb/product/exception/errors/ProductNotFoundException.java
@@ -1,12 +1,14 @@
 package kr.bb.product.exception.errors;
 
-import kr.bb.product.exception.errors.common.EntityNotFoundException;
 
-public class ProductNotFoundException extends EntityNotFoundException {
+import bloomingblooms.errors.BaseException;
+import bloomingblooms.errors.ErrorCode;
+
+public class ProductNotFoundException extends BaseException {
 
   private static final String message = "상품이 존재하지 않습니다.";
 
   public ProductNotFoundException() {
-    super(message);
+    super(ErrorCode.valueOf(message));
   }
 }
--- src/main/java/kr/bb/product/exception/errors/common/DomainException.java
+++
@@ -1,8 +0,0 @@
-package kr.bb.product.exception.errors.common;
-
-public class DomainException extends BaseException {
-
-  public DomainException(String message, ErrorCode errorCode) {
-    super(message, errorCode);
-  }
-}
