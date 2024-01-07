e14e5d408c6c2f7395cb83bc502f106fe3fb918d
nowgnas <dev.nowgnas@gmail.com>
2024-01-07 23:28:06 +0900

    :recycle: Refactor : kafka message string serializer

--- src/main/java/kr/bb/product/domain/product/infrastructure/event/ProductKafkaListener.java
+++ src/main/java/kr/bb/product/domain/product/infrastructure/event/ProductKafkaListener.java
@@ -1,6 +1,8 @@
 package kr.bb.product.domain.product.infrastructure.event;
 
 import bloomingblooms.domain.order.ProcessOrderDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.bb.product.domain.product.application.handler.ProductQueryHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProductKafkaListener {
   private final ProductQueryHandler productQueryHandler;
+  private final ObjectMapper objectMapper;
 
   @KafkaListener(topics = "stock-decrease", groupId = "stock-decrease")
-  public void stockDecreaseRequestListener(ProcessOrderDto processOrderDto) {
+  public void stockDecreaseRequestListener(String message) throws JsonProcessingException {
+    ProcessOrderDto processOrderDto = objectMapper.readValue(message, ProcessOrderDto.class);
     productQueryHandler.getFlowerStockDecrease(processOrderDto);
   }
 
   @KafkaListener(topics = "order-create-rollback", groupId = "order-rollback")
-  public void orderRollbackRequestListener(ProcessOrderDto processOrderDto) {
+  public void orderRollbackRequestListener(String message) throws JsonProcessingException {
+    ProcessOrderDto processOrderDto = objectMapper.readValue(message, ProcessOrderDto.class);
     productQueryHandler.getFlowerStockRollback(processOrderDto);
   }
 }
--- src/main/java/kr/bb/product/domain/product/infrastructure/event/ProductKafkaProcessor.java
+++ src/main/java/kr/bb/product/domain/product/infrastructure/event/ProductKafkaProcessor.java
@@ -1,5 +1,7 @@
 package kr.bb.product.domain.product.infrastructure.event;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -7,9 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ProductKafkaProcessor<T> {
-  private final KafkaTemplate<String, T> kafkaTemplate;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  private final ObjectMapper objectMapper;
 
   public void send(String topicName, T data) {
-    kafkaTemplate.send(topicName, data);
+    try {
+      String message = objectMapper.writeValueAsString(data);
+      kafkaTemplate.send(topicName, message);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
